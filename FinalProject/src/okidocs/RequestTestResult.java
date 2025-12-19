459e86b10868e135b28c394977cc604485d77000
Percie Louise Y. Samaniego <155746264+perky-boo25@users.noreply.github.com>
2025-12-19 18:06:12 +0800

    Add RequestTestResult utility for test result requests
    RequestTestResult class with a static method to handle student test result requests, including login checks and user feedback dialogs.

---
+++ FinalProject/src/okidocs/RequestTestResult.java
@@ -0,0 +1,27 @@
+package okidocs;
+
+import javax.swing.*;
+
+public class RequestTestResult {
+    
+    public static void request(JPanel parent){
+        
+        int studentId = Session.getStudentId();
+
+        if (studentId == -1){
+            JOptionPane.showMessageDialog(parent, "Please login first.");
+            return;
+        }
+
+        //TEMP: TO BE CHANGED
+        boolean requested = TestResultDAO.requestTestResult(studentId);
+
+        if(requested){
+            JOptionPane.showMessageDialog(parent, "Request submitted. \n Results will be available in 5 to 10 business days.");
+        } else {
+            JOptionPane.showMessageDialog(parent, "You have already requested your test result.");
+        }
+
+
+    }
+}
