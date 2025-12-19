a166a1e6fe47b9fdaf1af3ec726a7da6f1c23bcf
Percie Louise Y. Samaniego <155746264+perky-boo25@users.noreply.github.com>
2025-12-19 21:35:27 +0800

    Add TestResultDAO class with initial methods for request and release status

---
+++ FinalProject/src/okidocs/TestResultDAO.java
@@ -0,0 +1,10 @@
+package okidocs; 
+
+public class TestResultDAO {
+    public static boolean hasRequested(int id) { 
+        return true; 
+    }
+    public static boolean isReleased(int id) {
+         return true; 
+    }
+}
